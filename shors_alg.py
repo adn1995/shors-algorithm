ac740879fb30ab71974d3f03961ff75e444d18b4
Arthur Diep-Nguyen <github@arthurprofessional.aleeas.com>
2025-06-08 20:44:04 -0700

    Write main body for oracle

--- shors_alg.py
+++ shors_alg.py
@@ -26,13 +26,62 @@ def oracle(a: int, N: int) -> QuantumCircuit:
     -------
     QuantumCircuit
         Controlled circuit CU_a given by
-            CU_a |x>_1 |y>_n
-        that returns |x>_1 |a*y mod N>_n when x=1,
-        where n = 2*ceil(log2(N))
+            CU_a |c>_1 |x>_n
+        that returns |c>_1 |a*x mod N>_n when c=1,
+        where n = ceil(log2(N))
     """
     #TODO see Figure 7
     #TODO don't forget to use to_gate() when composing subcircuits
-    pass
+
+    # Number of bits required to represent N
+    n = ceil(log2(N))
+
+    # One control qubit
+    control_qr = QuantumRegister(1, name="c")
+
+    # n qubits for |x>, the n-qubit input for CMULT(a)MOD(N)
+    input_qr = QuantumRegister(n, name="x")
+
+    # n+2 qubits
+    # n+1 qubits are for (n+1)-qubit input |b> for CMULT(a)MOD(N)
+    # The last qubit is the ancillary qubit
+    ancilla = AncillaRegister(n+2, name="a")
+
+    qc = QuantumCircuit(control_qr, input_qr, ancilla, name="oracle")
+
+    # Controlled multiplier
+    qc.compose(c_mult_mod(a,N),
+                qubits=[control_qr, input_qr, ancilla],
+                inplace=True)
+
+
+    # CMULT(a)MOD(N) has the following output wires
+    #   |c>             1 control qubit
+    #   |x>             n qubits
+    #   |b+a*x mod N>   n+1 qubits
+    #   |0>             1 ancillary qubit from ADD(a)MOD(N)
+    #
+    # We want to controlled-swap |x> with |b+a*x mod N>,
+    # controlled by |c>.
+    #
+    # Since b=0, we have |b+a*x mod N> = |ax mod N>, which requires
+    # only n qubits, not n+1 qubits, so we can ignore the most
+    # significant qubit of |b+a*x mod N>.
+    #
+    # In other words, we want to controlled-swap the n qubits of |x>
+    # with the n least significant qubits of |b+a*x mod N>.
+    for i in range(n):
+        qc.cswap(control_qubit=control_qr,
+            target_qubit1=input_qr[i],
+            target_qubit2=ancilla[i+1])
+
+    # Inverse controlled multiplier
+
+    qc.compose(c_mult_mod(a,N).inverse(),
+                qubits=[control_qr, input_qr, ancilla],
+                inplace=True)
+
+    return qc
 
 ########################################################################
 # Subcircuits
@@ -49,9 +98,6 @@ def cc_adder_mod(a: int, N: int) -> QuantumCircuit:
 def c_mult_mod(a: int, N: int) -> QuantumCircuit:
     #TODO controlled, see Section 2.3 and Figure 6
     pass
-
-#TODO we also need to write a controlled swap circuit, since we are
-# probably not allowed to use the built in swap gate :(
 
 ########################################################################
 # Other helper functions
